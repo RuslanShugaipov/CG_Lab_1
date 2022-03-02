2237fd50ad8cd4f1e76ae4caf4916f42f07d59c4
DESKTOP-S3L4SGI\Руслан <nabon1812@mail.ru>
2022-03-02 14:23:34 +0300

    Added fixes.

--- Lab_1/Form1.Designer.cs
+++ Lab_1/Form1.Designer.cs
@@ -32,14 +32,22 @@ namespace Lab_1
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(51, 412);
+            this.button1.Location = new System.Drawing.Point(27, 364);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(147, 46);
+            this.button1.Size = new System.Drawing.Size(150, 50);
             this.button1.TabIndex = 0;
             this.button1.Text = "RotateY";
             this.button1.UseVisualStyleBackColor = true;
@@ -48,29 +56,92 @@ namespace Lab_1
             // pictureBox1
             // 
             this.pictureBox1.BackColor = System.Drawing.SystemColors.Window;
-            this.pictureBox1.Location = new System.Drawing.Point(51, 34);
+            this.pictureBox1.Location = new System.Drawing.Point(27, 21);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(550, 350);
+            this.pictureBox1.Size = new System.Drawing.Size(250, 250);
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
-            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(51, 464);
+            this.button2.Location = new System.Drawing.Point(27, 416);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(147, 46);
+            this.button2.Size = new System.Drawing.Size(150, 50);
             this.button2.TabIndex = 2;
             this.button2.Text = "RotateZ";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox2.Location = new System.Drawing.Point(375, 21);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(27, 308);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(150, 50);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "RotateX";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(201, 364);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(100, 50);
+            this.button4.TabIndex = 6;
+            this.button4.Text = "Dimetric\r\nprojection";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(201, 416);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(100, 50);
+            this.button5.TabIndex = 7;
+            this.button5.Text = "Isometric\r\nprojection";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(201, 308);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(100, 50);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Draw\r\ncube";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.pictureBox3.Location = new System.Drawing.Point(375, 299);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox3.TabIndex = 9;
+            this.pictureBox3.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ScrollBar;
-            this.ClientSize = new System.Drawing.Size(656, 532);
+            this.ClientSize = new System.Drawing.Size(655, 561);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
@@ -78,6 +149,8 @@ namespace Lab_1
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -87,6 +160,12 @@ namespace Lab_1
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- Lab_1/Form1.cs
+++ Lab_1/Form1.cs
@@ -19,37 +19,96 @@ namespace Lab_1
             InitializeComponent();
             Width = 500;
             Height = 500;
+            pictureBox1.Image = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox2.Image = new Bitmap(pictureBox2.Width, pictureBox2.Height);
+            pictureBox3.Image = new Bitmap(pictureBox3.Width, pictureBox3.Height);
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+
         }
 
+        double angleX = 0;
         double angleY = 0;
         double angleZ = 0;
+        bool is_draw_dimetric = false;
+        bool is_draw_isometric = false;
         private void button1_Click(object sender, EventArgs e)
         {
             angleY += 5;
+            button6_Click(sender, e);
         }
 
         private void button2_Click_1(object sender, EventArgs e)
         {
             angleZ += 5;
+            button6_Click(sender, e);
         }
 
-        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        private void button3_Click(object sender, EventArgs e)
         {
-            pictureBox1.Invalidate();
-            float centerX = (float)pictureBox1.Width / 2;
-            float centerY = (float)pictureBox1.Height / 2;
-            e.Graphics.TranslateTransform(centerX, centerY);
-            Cube cube = new Cube(50.0f, new int[] { 0, 0, 0 });
-            Pen pen = new Pen(Brushes.Red);
-            //cube.translation(offset, 0.0f, 0.0f);
-            cube.rotationY(angleY);
-            cube.rotationZ(angleZ);
-            cube.Draw(e.Graphics, pen);
+            angleX += 5;
+            button6_Click(sender, e);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            is_draw_dimetric = !is_draw_dimetric;
+            using (var g = Graphics.FromImage(pictureBox2.Image))
+            {
+                g.Clear(pictureBox2.BackColor);
+                pictureBox2.Invalidate();
+                float centerX = (float)pictureBox1.Width / 2;
+                float centerY = (float)pictureBox1.Height / 2;
+                g.TranslateTransform(centerX, centerY);
+                Cube cube_dimetric = new Cube(50.0f, new int[] { 0, 0, 0 });
+                Pen pen = new Pen(Brushes.Red);
+                cube_dimetric.rotationY(22.208);
+                cube_dimetric.rotationX(20.705);
+                cube_dimetric.ortho(2.0);
+                if (is_draw_dimetric)
+                    cube_dimetric.Draw(g, pen);
+            }
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            is_draw_isometric = !is_draw_isometric;
+            using (var g = Graphics.FromImage(pictureBox3.Image))
+            {
+                g.Clear(pictureBox3.BackColor);
+                pictureBox3.Invalidate();
+                float centerX = (float)pictureBox3.Width / 2;
+                float centerY = (float)pictureBox3.Height / 2;
+                g.TranslateTransform(centerX, centerY);
+                Cube cube_isometric = new Cube(50.0f, new int[] { 0, 0, 0 });
+                Pen pen = new Pen(Brushes.Red);
+                cube_isometric.rotationY(45.0);
+                cube_isometric.rotationX(35.26);
+                cube_isometric.ortho(0.8165);
+                if (is_draw_isometric)
+                    cube_isometric.Draw(g, pen);
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (var g = Graphics.FromImage(pictureBox1.Image))
+            {
+                g.Clear(pictureBox1.BackColor);
+                pictureBox1.Invalidate();
+                float centerX = (float)pictureBox1.Width / 2;
+                float centerY = (float)pictureBox1.Height / 2;
+                g.TranslateTransform(centerX, centerY);
+                Cube cube = new Cube(50.0f, new int[] { 0, 0, 0 });
+                Pen pen = new Pen(Brushes.Red);
+                cube.rotationX(angleX);
+                cube.rotationY(angleY);
+                cube.rotationZ(angleZ);
+                cube.Draw(g, pen);
+            }
         }
     }
 
@@ -98,11 +157,25 @@ namespace Lab_1
                 vertecies[i] = vertecies[i] * ortho;
             }
         }
+        public void rotationX(double angle)
+        {
+            angle = angle * (Math.PI / 180);
+            Matrix rotationX = new Matrix(new float[4, 4]{
+                {    1,                         0,                         0, 0 },
+                {    0,    (float)Math.Cos(angle),    (float)Math.Sin(angle), 0 },
+                {    0, -((float)Math.Sin(angle)),    (float)Math.Cos(angle), 0 },
+                {    0,                         0,                         0, 1 }
+            });
+            for (int i = 0; i < 8; ++i)
+            {
+                vertecies[i] = vertecies[i] * rotationX;
+            }
+        }
 
         public void rotationY(double angle)
         {
             angle = angle * (Math.PI / 180);
-            Matrix rotationZ = new Matrix(new float[4, 4]{
+            Matrix rotationY = new Matrix(new float[4, 4]{
                 {    (float)Math.Cos(angle), 0, -((float)Math.Sin(angle)), 0 },
                 {                         0, 1,                         0, 0 },
                 {    (float)Math.Sin(angle), 0,    (float)Math.Cos(angle), 0 },
@@ -110,7 +183,7 @@ namespace Lab_1
             });
             for (int i = 0; i < 8; ++i)
             {
-                vertecies[i] = vertecies[i] * rotationZ;
+                vertecies[i] = vertecies[i] * rotationY;
             }
         }
 
